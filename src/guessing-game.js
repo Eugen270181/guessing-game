4da7e34784428d27e9fd55f9be7e0e4260c1a056
Eugen Sadovnikov <sadeugen@gmail.com>
2023-10-27 22:52:33 +0300

    feat: feat all

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        let min = 0;
+        let max = 0;
+        let middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return this.middle = Math.ceil((this.min+this.max)/2);    
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
